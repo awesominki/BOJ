2fc751af927bbe108821e7d72a7154a31383d90a
mac <oversteam@naver.com>
2023-04-12 16:39:19 +0900

    BOJ N과M

---
+++ N과 M(3)(15651).py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+ans = []
+
+def dfs():
+    if len(ans) == m:
+        print(' '.join(map(str,ans))) #숫자가 들어있는 리스트는 str을 통해 문자로 바꿔서 출력해야함!
+        return
+    for i in range(1,n+1):
+        ans.append(i)
+        dfs()
+        ans.pop()
+
+
+dfs()
